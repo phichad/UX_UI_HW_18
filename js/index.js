6c61e00bf13748b84841c329035c6996fce999e1
phichad <philixchadbourn@gmail.com>
2022-08-16 11:08:56 -0500

    updated buttons

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(".button").on("click", function(){
+    console.log("you clicked a button!");
+});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
